04f56a07f425ab013e13324710b8a20456dcf0a8
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2021-02-26 05:44:23 +0000

    update TestAssignHomes for new register allocation

--- asm_test.go
+++ asm_test.go
@@ -107,15 +107,20 @@ func TestAssignHomes(t *testing.T) {
 		label: "L0",
 		args:  nil,
 		code: []asmOp{
-			{tag: asmInstr, variant: "subq", args: []asmArg{{Reg: "rsp"}, {Imm: 16}}},
-			{tag: asmInstr, variant: "movq", args: []asmArg{mkmem("rsp", 0), {Imm: 20}}},
-			{tag: asmInstr, variant: "movq", args: []asmArg{mkmem("rsp", 8), {Imm: 2}}},
-			{tag: asmInstr, variant: "addq", args: []asmArg{mkmem("rsp", 0), mkmem("rsp", 0)}},
-			{tag: asmInstr, variant: "addq", args: []asmArg{mkmem("rsp", 0), mkmem("rsp", 8)}},
-			{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rax"}, mkmem("rsp", 0)}},
-			{tag: asmInstr, variant: "addq", args: []asmArg{{Reg: "rsp"}, {Imm: 16}}},
+			//{tag: asmInstr, variant: "subq", args: []asmArg{{Reg: "rsp"}, {Imm: 16}}},
+			//{tag: asmInstr, variant: "movq", args: []asmArg{mkmem("rsp", 0), {Imm: 20}}},
+			//{tag: asmInstr, variant: "movq", args: []asmArg{mkmem("rsp", 8), {Imm: 2}}},
+			//{tag: asmInstr, variant: "addq", args: []asmArg{mkmem("rsp", 0), mkmem("rsp", 0)}},
+			//{tag: asmInstr, variant: "addq", args: []asmArg{mkmem("rsp", 0), mkmem("rsp", 8)}},
+			//{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rax"}, mkmem("rsp", 0)}},
+			//{tag: asmInstr, variant: "addq", args: []asmArg{{Reg: "rsp"}, {Imm: 16}}},
+			{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rcx"}, {Imm: 20}}},
+			{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rdx"}, {Imm: 2}}},
+			{tag: asmInstr, variant: "addq", args: []asmArg{{Reg: "rcx"}, {Reg: "rcx"}}},
+			{tag: asmInstr, variant: "addq", args: []asmArg{{Reg: "rcx"}, {Reg: "rdx"}}},
+			{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rax"}, {Reg: "rcx"}}},
 		},
-		stacksize: 16,
+		stacksize: 0,
 	}
 	if !reflect.DeepEqual(block, expected) {
 		fmt.Println("got:")
