147ad78a8e6883af61f5e795e2109f0776a50523
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2021-02-26 05:57:46 +0000

    remove asmTests table

--- asm_test.go
+++ asm_test.go
@@ -8,17 +8,15 @@ import (
 	"testing"
 )
 
-var asmTests = []struct {
-	block  asmBlock
-	output string
-}{
-	{asmBlock{
+func TestAsmPrinter(t *testing.T) {
+	block := asmBlock{
 		label: "L0",
 		code: []asmOp{
 			{tag: asmInstr, variant: "movq", args: []asmArg{{Reg: "rax"}, {Imm: 10}}},
 			{tag: asmInstr, variant: "addq", args: []asmArg{{Reg: "rax"}, {Imm: 2}}},
 		},
-	}, `
+	}
+	want := `
 	.globl psc_main
 psc_main:
 	pushq %rbp
@@ -29,18 +27,15 @@ psc_main:
 
 	popq %rbp
 	ret
-`}}
+`
 
-func TestAsmPrinter(t *testing.T) {
-	for _, tt := range asmTests {
-		var p AsmPrinter
-		buf := new(bytes.Buffer)
-		p.w = buf
-		p.ConvertBlock(&tt.block)
-		got := buf.String()
-		if got != tt.output {
-			t.Errorf("output didn't match\nblock: %+v\nexpected:\n%s\nactual:\n%s", tt.block, tt.output, got)
-		}
+	var p AsmPrinter
+	buf := new(bytes.Buffer)
+	p.w = buf
+	p.ConvertBlock(&block)
+	got := buf.String()
+	if got != want {
+		t.Errorf("asm output didn't match\nexpected:\n%s\nactual:\n%s", want, got)
 	}
 }
 
